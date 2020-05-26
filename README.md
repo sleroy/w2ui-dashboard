0030e79288428d3c0e2c90c943f5d6307940d193
Sylvain Leroy <sylvain.leroy@salt.ch>
2020-05-26 17:35:14 +0200

    Asset copy supported

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Template for a basic w2ui dashboard
 
 
 # References
+
+* https://webpack.js.org/guides/typescript/
+* https://www.digitalocean.com/community/tutorials/typescript-new-project
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    /*new HtmlWebpackPlugin({
+      title: "Output Management",
+    }),*/
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.' },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "js/bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
