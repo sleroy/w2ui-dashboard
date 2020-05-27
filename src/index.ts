468c936d3d125ad7d5934ff9bbb52c066d44027e
Sylvain Leroy <sylvain.leroy@salt.ch>
2020-05-27 18:13:36 +0200

    Integration w2ui and TS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import W2UI from 'w2ui';
-import $ from "jquery";
-
-const world = '🗺️';
-
-export function hello(word: string = world): string {
-    return `Hello ${world}! `;
-}
+$(function () {
+    const pstyle = 'border: 1px solid #dfdfdf; padding: 5px;';
+    $('#layout').w2layout({
+        name: 'layout',
+        panels: [
+            { type: 'top', size: 50, style: pstyle, content: 'top' },
+            { type: 'left', size: 200, style: pstyle, content: 'left' },
+            { type: 'main', style: pstyle, content: 'main' },
+        ],
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,48 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.ts",
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    /*new HtmlWebpackPlugin({
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        /*new HtmlWebpackPlugin({
       title: "Output Management",
     }),*/
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'public', to: '.' },
-        { from: 'node_modules/w2ui/w2ui.css', to: 'css' },
-      ],
-    }),
-  ],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "js/bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public', to: '.' },
+                { from: 'node_modules/w2ui/w2ui.min.css', to: 'css' },
+                { from: 'node_modules/w2ui/w2ui.min.js', to: 'js' },
+                { from: 'node_modules/jquery/dist/jquery.min.js', to: 'js' },
+            ],
+        }),
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'js/bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
