1b36e5a136e319b7fd40de88f85ce44a996088ad
Sylvain Leroy <sylvain.leroy@salt.ch>
2020-05-27 18:19:46 +0200

    Adding toolbar sample

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,57 @@ $(function () {
         panels: [
             { type: 'top', size: 50, style: pstyle, content: 'top' },
             { type: 'left', size: 200, style: pstyle, content: 'left' },
-            { type: 'main', style: pstyle, content: 'main' },
+            {
+                type: 'main',
+                style: pstyle + 'border-top: 0px;',
+                content: 'content',
+                toolbar: {
+                    items: [
+                        { type: 'check', id: 'item1', caption: 'Check', img: 'icon-page', checked: true },
+                        { type: 'break', id: 'break0' },
+                        {
+                            type: 'menu',
+                            id: 'item2',
+                            caption: 'Drop Down',
+                            img: 'icon-folder',
+                            items: [
+                                { text: 'Item 1', icon: 'icon-page' },
+                                { text: 'Item 2', icon: 'icon-page' },
+                                { text: 'Item 3', value: 'Item Three', icon: 'icon-page' },
+                            ],
+                        },
+                        { type: 'break', id: 'break1' },
+                        {
+                            type: 'radio',
+                            id: 'item3',
+                            group: '1',
+                            caption: 'Radio 1',
+                            img: 'icon-page',
+                            hint: 'Hint for item 3',
+                            checked: true,
+                        },
+                        {
+                            type: 'radio',
+                            id: 'item4',
+                            group: '1',
+                            caption: 'Radio 2',
+                            img: 'icon-page',
+                            hint: 'Hint for item 4',
+                        },
+                        { type: 'spacer' },
+                        {
+                            type: 'button',
+                            id: 'item5',
+                            caption: 'Item 5',
+                            icon: 'w2ui-icon-check',
+                            hint: 'Hint for item 5',
+                        },
+                    ],
+                    onClick: function (event:any) {
+                      //
+                    },
+                },
+            },
         ],
     });
 });
